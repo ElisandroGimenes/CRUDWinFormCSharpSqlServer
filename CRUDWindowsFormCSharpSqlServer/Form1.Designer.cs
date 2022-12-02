792f4d8b6d1cec6ab30c5a96f2b0b7453abcde15
Elisandro <Elisandro@LAPTOP-CCH60LK6>
2022-12-02 09:46:36 -0300

    Adicionar arquivos de projeto.

---
+++ CRUDWindowsFormCSharpSqlServer/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace CRUDWindowsFormCSharpSqlServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDlbl = new System.Windows.Forms.Label();
+            this.NomeLbl = new System.Windows.Forms.Label();
+            this.TelefoneLbl = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.BtnSalvar = new System.Windows.Forms.Button();
+            this.BtnEditar = new System.Windows.Forms.Button();
+            this.BtnConsultar = new System.Windows.Forms.Button();
+            this.BtnExibir = new System.Windows.Forms.Button();
+            this.BtnExcluir = new System.Windows.Forms.Button();
+            this.DgvDados = new System.Windows.Forms.DataGridView();
+            this.EmailLbl = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IDlbl
+            // 
+            this.IDlbl.AutoSize = true;
+            this.IDlbl.Location = new System.Drawing.Point(33, 41);
+            this.IDlbl.Name = "IDlbl";
+            this.IDlbl.Size = new System.Drawing.Size(20, 16);
+            this.IDlbl.TabIndex = 0;
+            this.IDlbl.Text = "ID";
+            // 
+            // NomeLbl
+            // 
+            this.NomeLbl.AutoSize = true;
+            this.NomeLbl.Location = new System.Drawing.Point(33, 77);
+            this.NomeLbl.Name = "NomeLbl";
+            this.NomeLbl.Size = new System.Drawing.Size(44, 16);
+            this.NomeLbl.TabIndex = 1;
+            this.NomeLbl.Text = "Nome";
+            // 
+            // TelefoneLbl
+            // 
+            this.TelefoneLbl.AutoSize = true;
+            this.TelefoneLbl.Location = new System.Drawing.Point(33, 150);
+            this.TelefoneLbl.Name = "TelefoneLbl";
+            this.TelefoneLbl.Size = new System.Drawing.Size(61, 16);
+            this.TelefoneLbl.TabIndex = 2;
+            this.TelefoneLbl.Text = "Telefone";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(166, 34);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(275, 22);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(166, 70);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(275, 22);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(166, 143);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(275, 22);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // BtnSalvar
+            // 
+            this.BtnSalvar.Location = new System.Drawing.Point(36, 187);
+            this.BtnSalvar.Name = "BtnSalvar";
+            this.BtnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.BtnSalvar.TabIndex = 6;
+            this.BtnSalvar.Text = "Salvar";
+            this.BtnSalvar.UseVisualStyleBackColor = true;
+            this.BtnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.Location = new System.Drawing.Point(153, 186);
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(75, 23);
+            this.BtnEditar.TabIndex = 7;
+            this.BtnEditar.Text = "Editar";
+            this.BtnEditar.UseVisualStyleBackColor = true;
+            this.BtnEditar.Click += new System.EventHandler(this.BtnEditar_Click);
+            // 
+            // BtnConsultar
+            // 
+            this.BtnConsultar.Location = new System.Drawing.Point(269, 186);
+            this.BtnConsultar.Name = "BtnConsultar";
+            this.BtnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.BtnConsultar.TabIndex = 8;
+            this.BtnConsultar.Text = "Consultar";
+            this.BtnConsultar.UseVisualStyleBackColor = true;
+            this.BtnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // BtnExibir
+            // 
+            this.BtnExibir.Location = new System.Drawing.Point(389, 186);
+            this.BtnExibir.Name = "BtnExibir";
+            this.BtnExibir.Size = new System.Drawing.Size(75, 23);
+            this.BtnExibir.TabIndex = 9;
+            this.BtnExibir.Text = "Exibir";
+            this.BtnExibir.UseVisualStyleBackColor = true;
+            this.BtnExibir.Click += new System.EventHandler(this.BtnExibir_Click);
+            // 
+            // BtnExcluir
+            // 
+            this.BtnExcluir.Location = new System.Drawing.Point(518, 186);
+            this.BtnExcluir.Name = "BtnExcluir";
+            this.BtnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.BtnExcluir.TabIndex = 10;
+            this.BtnExcluir.Text = "Excluir";
+            this.BtnExcluir.UseVisualStyleBackColor = true;
+            this.BtnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // DgvDados
+            // 
+            this.DgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvDados.Location = new System.Drawing.Point(36, 224);
+            this.DgvDados.Name = "DgvDados";
+            this.DgvDados.RowHeadersWidth = 51;
+            this.DgvDados.RowTemplate.Height = 24;
+            this.DgvDados.Size = new System.Drawing.Size(557, 209);
+            this.DgvDados.TabIndex = 11;
+            // 
+            // EmailLbl
+            // 
+            this.EmailLbl.AutoSize = true;
+            this.EmailLbl.Location = new System.Drawing.Point(33, 113);
+            this.EmailLbl.Name = "EmailLbl";
+            this.EmailLbl.Size = new System.Drawing.Size(45, 16);
+            this.EmailLbl.TabIndex = 12;
+            this.EmailLbl.Text = "E-mail";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(166, 107);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(275, 22);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.EmailLbl);
+            this.Controls.Add(this.DgvDados);
+            this.Controls.Add(this.BtnExcluir);
+            this.Controls.Add(this.BtnExibir);
+            this.Controls.Add(this.BtnConsultar);
+            this.Controls.Add(this.BtnEditar);
+            this.Controls.Add(this.BtnSalvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.TelefoneLbl);
+            this.Controls.Add(this.NomeLbl);
+            this.Controls.Add(this.IDlbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IDlbl;
+        private System.Windows.Forms.Label NomeLbl;
+        private System.Windows.Forms.Label TelefoneLbl;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button BtnSalvar;
+        private System.Windows.Forms.Button BtnEditar;
+        private System.Windows.Forms.Button BtnConsultar;
+        private System.Windows.Forms.Button BtnExibir;
+        private System.Windows.Forms.Button BtnExcluir;
+        private System.Windows.Forms.DataGridView DgvDados;
+        private System.Windows.Forms.Label EmailLbl;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
+
---
+++ CRUDWindowsFormCSharpSqlServer/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CRUDWindowsFormCSharpSqlServer
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        SqlDataAdapter da;
+
+        String strSql;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            //String conexão com banco de dados
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-CCH60LK6;Initial Catalog=teste;Integrated Security=True");
+
+            //String de inserção no banco de dados
+            string strSql = "INSERT INTO pessoas (id, nome, email, telefone) VALUES (@id, @nome, @email, @telefone)";
+            Random numeroID = new Random();
+            numeroID.Next();
+
+            try
+            {
+                //Cria um objeto do tipo comando passando com parâmetros as strings sql e conn 
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                //Insere os dados  da txtBox no comando sql
+                cmd.Parameters.Add(new SqlParameter("@id", numeroID.Next()));
+                cmd.Parameters.Add(new SqlParameter("@nome", this.txtNome.Text));
+                cmd.Parameters.Add(new SqlParameter("@email", this.txtEmail.Text));
+                cmd.Parameters.Add(new SqlParameter("@telefone", this.txtTelefone.Text));
+
+                //Abre a conexão com o banco
+                conn.Open();
+
+                //Executa o comando Sql
+                cmd.ExecuteNonQuery();
+
+                //Fecha a conexão com o banco
+                conn.Close();
+
+                MessageBox.Show("Registro salvo com sucesso!");
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro de conexão: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void BtnExibir_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                //String conexão com banco de dados
+                SqlConnection conn = new SqlConnection("Data Source=LAPTOP-CCH60LK6;Initial Catalog=teste;Integrated Security=True");
+
+                //String de inserção no banco de dados
+                string strSql = "SELECT * FROM pessoas"; 
+                
+                DataSet ds = new DataSet();
+
+                da = new SqlDataAdapter(strSql, conn);
+               
+                //Cria um objeto do tipo comando passando com parâmetros as strings sql e conn 
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                //Abre a conexão com o banco
+                conn.Open();
+
+                //Preenche os campos
+                da.Fill(ds);
+
+                //Exibe no data gride view
+                DgvDados.DataSource = ds.Tables[0];
+
+                //Fecha a conexão com o banco
+                conn.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro de conexão: " + ex);
+            }
+            finally
+            {           
+                conn= null;
+            }
+        }
+
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            //String conexão com banco de dados
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-CCH60LK6;Initial Catalog=teste;Integrated Security=True");
+
+            //String de inserção no banco de dados
+            string strSql = "SELECT * FROM pessoas WHERE Id = @id";
+            
+            try
+            {
+                //Cria um objeto do tipo comando passando com parâmetros as strings sql e conn 
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                //Insere os dados  da txtBox no comando sql
+                cmd.Parameters.AddWithValue("@id", txtID.Text);
+               
+                //Abre a conexão com o banco
+                conn.Open();
+
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    txtNome.Text = (string)reader["nome"];
+                    txtEmail.Text = (string)reader["email"];
+                    txtTelefone.Text = Convert.ToString(reader["telefone"]);
+                }               
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro de conexão: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            //String conexão com banco de dados
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-CCH60LK6;Initial Catalog=teste;Integrated Security=True");
+
+            //String de inserção no banco de dados
+            string strSql = "UPDATE pessoas SET nome = @nome, email = @email, telefone = @telefone WHERE id = @id";
+
+            try
+            {
+                //Cria um objeto do tipo comando passando com parâmetros as strings sql e conn 
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                //Insere os dados  da txtBox no comando sql
+                cmd.Parameters.Add(new SqlParameter("@id", this.txtID.Text));
+                cmd.Parameters.Add(new SqlParameter("@nome", this.txtNome.Text));
+                cmd.Parameters.Add(new SqlParameter("@email", this.txtEmail.Text));
+                cmd.Parameters.Add(new SqlParameter("@telefone", this.txtTelefone.Text));
+
+                //Abre a conexão com o banco
+                conn.Open();
+
+                //Executa o comando Sql
+                cmd.ExecuteNonQuery();
+
+                //Fecha a conexão com o banco
+                conn.Close();
+
+                MessageBox.Show("Alteração salva com sucesso!");
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro de conexão: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            //String conexão com banco de dados
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-CCH60LK6;Initial Catalog=teste;Integrated Security=True");
+
+            //String de inserção no banco de dados
+            string strSql = "DELETE pessoas WHERE id = @id";
+          
+            try
+            {
+                //Cria um objeto do tipo comando passando com parâmetros as strings sql e conn 
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+
+                //Insere os dados  da txtBox no comando sql
+                cmd.Parameters.Add(new SqlParameter("@id", txtID.Text));
+                
+                //Abre a conexão com o banco
+                conn.Open();
+
+                //Executa o comando Sql
+                cmd.ExecuteNonQuery();
+
+                //Fecha a conexão com o banco
+                conn.Close();
+
+                MessageBox.Show("Registro excluído com sucesso!");
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro de conexão: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
+
+
+
